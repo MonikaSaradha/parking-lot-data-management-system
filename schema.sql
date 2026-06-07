98d66d4909d10f26aee3fe5110c8ed4d699da2ee
Monika Saradha <109618220+MonikaSaradha@users.noreply.github.com>
2026-06-07 22:26:26 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE vehicle_category (
+  vehicle_id INT NOT NULL AUTO_INCREMENT,
+  vehicle_category VARCHAR(50) NOT NULL,
+  user_group_name VARCHAR(50) NOT NULL,
+  plate_number VARCHAR(20) NOT NULL UNIQUE,
+  status VARCHAR(10) NOT NULL,
+  PRIMARY KEY (vehicle_id)
+);
