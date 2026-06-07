c6965cfb71ed394f2c7a426c1c3a48f4ba2fcb51
Monika Saradha <109618220+MonikaSaradha@users.noreply.github.com>
2026-06-07 22:26:47 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO vehicle_category (vehicle_category, user_group_name, plate_number, status) VALUES 
+('Car', 'Employees', 'ABC-123', 'Active'),
+('Truck', 'Contractors', 'XYZ-789', 'Inactive'),
+('Motorcycle', 'Visitors', 'DEF-456', 'Active'),
+('Bus', 'Students', 'GHI-789', 'Active'),
+('Van', 'Employees', 'JKL-012', 'Inactive'),
+('Cycle', 'Students', 'JUN-666', 'Active'),
+('Cycle', 'Students', 'FYN-524', 'Active'),
+('Bus', 'Visitors', 'QER-342', 'Active'),
+('Car', 'Students', 'BGT-534', 'Active'),
+('Car', 'Students', 'LKI-847', 'Active'),
+('Car', 'Employees', 'ATB-267', 'Active');
